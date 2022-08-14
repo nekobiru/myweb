e9a021970c2bcb257bec2bac39a275dde3860bb1
nekobiru <nekobirugame@gmail.com>
2022-08-14 18:52:43 +0700

    myweb

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <title>PCraft7265</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <link rel="stylesheet" href="assets/fontawesome-free-6.1.0-web/css/all.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="assets/fontawesome-free-6.1.0-web/css/all.min.css" type="text/css" media="all" />
+    <link rel="stylesheet" href="assets/css/pcraft7265.css" type="text/css" media="all" />
+    <link rel="shortcut icon" href="assets/profile.png" type="image/x-icon" />
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="container-flex">
+                <div class="brand">
+                    <a href="#">PCraft7265</a>
+                </div>
+                <div class="burger">
+                    <i class="fa fa-bars burgerc"></i>
+                </div>
+                <div class="bg-sidebar"></div>
+                <ul class="sidebar">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="login.php">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+        <div class="banner">
+            <img class="profileimg" src="assets/profile.png" alt="Profile Icon" />
+            <div class="wellcome">
+                <p>
+                    Hello, i'm
+                </p>
+                <h2>PCraft7265</h2>
+                <p>
+                    Wellcome to my web.This web only for my profile and i'm can't shared my project or other about me in this web
+                </p>
+            </div>
+        </div>
+    </header>
+    <section>
+        <div class="page">
+            <div class="card-post">
+                <img class="card-img" src="assets/8HJ.png" alt="Youtube" />
+                <h4 class="card-title"><a class="card-title" href="https://youtube.com/channel/UCyE34AyElckBb6Gr5ojv88g">Youtube</a></h4>
+            </div>
+            <div class="card-post-2">
+                <img class="card-img" src="assets/about_logo.png" alt="Whatsapp" />
+                <h4 class="card-title">Whatsapp</h4>
+            </div>
+        </div>
+        <div class="ppage">
+            <div class="card-post-2">
+                <img class="card-img" src="assets/cc-.png" alt="Instagram" />
+                <h4 class="card-title"><a class="card-title" href="https://www.instagram.com/pcraft7265/">Instagram</a></h4>
+            </div>
+            <div class="card-post">
+                <img class="card-img" src="assets/bsu.webp" alt="Facebook" />
+                <h4 class="card-title"><a class="card-title" href="https://www.facebook.com/profile.php?id=100070495700307">Instagram</a></h4>
+
+            </div>
+        </div>
+
+    </section>
+    <footer>
+        <p>
+            &copy;2022 PCraft7265
+        </p>
+    </footer>
+    <script src="assets/fontawesome-6.1.0-web/js/all.js.js" type="text/javascript" charset="utf-8"></script>
+    <script src="assets/fontawesome-6.1.0-web/js/all.min.js.js" type="text/javascript" charset="utf-8"></script>
+    <script src="assets/js/script.js" type="text/javascript" charset="utf-8"></script>
+</body>
+</html>
